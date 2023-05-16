58c323b21ea83e403996828ee0bd1cd32843a203
Verners Grikitis <vernersg@gmail.com>
2023-05-16 15:25:08 +0300

    "LeatestSecondHomeWorkCommit"

--- src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
+++ src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
@@ -10,37 +10,37 @@ public class TenBillion {
 
         long n = in.nextLong();
 
-        
+
         if (n < 0) {
             System.out.println("Number is negative ");
-        } else if (n >= 0 && n < 10) {
+        } else if (n < 10) {
             int digits = 1;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 10 && n < 99) {
+        } else if (n < 99) {
             int digits = 2;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 100 && n < 999) {
+        } else if (n < 999) {
             int digits = 3;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 1000 && n < 9999) {
+        } else if (n < 9999) {
             int digits = 4;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 10000 && n < 99999) {
+        } else if (n < 99999) {
             int digits = 5;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 100000 && n < 999999) {
+        } else if (n < 999999) {
             int digits = 6;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 1000000 && n < 9999999) {
+        } else if (n < 9999999) {
             int digits = 7;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 10000000 && n < 99999999) {
+        } else if (n < 99999999) {
             int digits = 8;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 100000000 && n < 999999999) {
+        } else if (n < 999999999) {
             int digits = 9;
             System.out.println("Number of digits in the number: " + digits);
-        } else if (n >= 1000000000 && n < 9999999999L) {
+        } else if (n < 9999999999L) {
             int digits = 10;
             System.out.println("Number of digits in the number: " + digits);
         } else if (n >= 10000000000L) {
--- src/main/java/io/codelex/loops/practice/Piglet.java
+++ src/main/java/io/codelex/loops/practice/Piglet.java
@@ -6,38 +6,35 @@ public class Piglet {
 
     public static void main(String[] args) {
 
-        int number = 1 + (int) (6 * Math.random());
-        int number2 = 1 + (int) (6 * Math.random());
-        int number3 = 1 + (int) (6 * Math.random());
-
-
+        int total = 0;
+        boolean rollAgain = true;
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Piglet! ");
-        System.out.println("You rolled a " + number + "!");
-        if (number == 1) {
-            System.out.println("You rolled 1! Game is 0ver! ");
-            System.exit(0);
+        System.out.println("If you want exit the game press any button and hit enter! ");
+        System.out.println("If you want continue game type `yes` or `y` and hit enter! ");
+
+        while (rollAgain) {
+            int roll = rollDice();
+            if (roll == 1) {
+                System.out.println("You rolled a 1!");
+                total = 0;
+                rollAgain = false;
+            } else {
+                System.out.println("You rolled a " + roll + "!");
+                total += roll;
+                System.out.println("You have a " + total + " points !");
+                System.out.println("Roll again? ");
+                rollAgain = readBoolean(input);
+            }
         }
-        System.out.print("If you want roll again press Enter ");
-        String firstAnswer = input.nextLine();
+    }
 
-        System.out.println("You rolled a " + number2 + "!");
-        if (number2 == 1) {
-            System.out.println("You rolled 1! Game is 0ver! ");
-            System.exit(0);
-        }
-        System.out.print("If you want roll again press Enter ");
-        String secondAnswer = input.nextLine();
+    public static int rollDice() {
+        return (int) (Math.random() * 6) + 1;
+    }
 
-        System.out.println("You rolled a " + number3 + "!");
-        if (number3 == 1) {
-            System.out.println("You rolled 1! Game is 0ver! ");
-            System.exit(0);
-        } else {
-            int points = number + number2 + number3;
-            System.out.println("You got " + points + " points.");
-        }
-
-
+    public static boolean readBoolean(Scanner input) {
+        String in = input.nextLine().toLowerCase();
+        return in.equals("yes") || in.equals("y");
     }
 }
